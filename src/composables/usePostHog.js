f33b6efe01e07f4441b694374004707790350b7e
FabienPochez <fabienpochez@me.com>
2025-12-02 13:07:51 +0100

    fix(analytics): migrate PostHog to EU server

--- src/composables/usePostHog.js
+++ src/composables/usePostHog.js
@@ -3,9 +3,10 @@ import posthog from 'posthog-js'
 export function usePostHog() {
   if (!posthog.__initialized) {
     posthog.__initialized = true
-    posthog.init('phc_MYLWM5FCKlXA33FACSWpVJNiyjSIhqXkXUg80JO3He', {
-      api_host: 'https://us.i.posthog.com',
-      person_profiles: 'identified_only'
+    posthog.init('phc_dXVNt4wf5Sgz6qHfMT5Zh1YQhNQTJLUTpuI1Y8sDjdM', {
+      api_host: 'https://eu.i.posthog.com',
+      defaults: '2025-11-30',
+      person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
     })
     posthog.register({ platform: 'webapp' })
   }
